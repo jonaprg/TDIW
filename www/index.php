58c24085d22aa3d02705d4da0ea3ab576257d69f
jonaprg <jonargp1@gmail.com>
2021-11-17 08:35:14 +0100

    Archivos de resource a www

--- www/index.php
+++ www/index.php
@@ -4,21 +4,21 @@ session_start();
 $request = $_GET['action'] ?? null;
 switch ($request) {
     case 'login' :
-        require __DIR__ . '/resource/resource_login.php';
+        require __DIR__ . '/resource_login.php';
         break;
     case 'register' :
-        require __DIR__ . '/resource/resource_register.php';
+        require __DIR__ . '/resource_register.php';
         break;
     case 'products' :
-        require __DIR__ . '/resource/resource_products.php';
+        require __DIR__ . '/resource_products.php';
         break;
     case 'productDetail' :
-        require __DIR__ . '/resource/resource_product_detail.php';
+        require __DIR__ . '/resource_product_detail.php';
         break;
     case 'logout' :
-        require __DIR__ .'/resource/resource_logout.php';
+        require __DIR__ . '/resource_logout.php';
         break;
     default:
-        require __DIR__ . '/resource/resource_portada.php';
+        require __DIR__ . '/resource_portada.php';
         break;
 }
